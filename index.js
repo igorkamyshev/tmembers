3d02b1e812e0ab04632c6a57e54b242c00d84978
Igor Kamyshev <igor@kamyshev.me>
2018-10-27 19:58:48 +0300

    Add fallback and travis setup

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
 
-const tmembers = channel => axios
+const tmembers = (channel, fallback) => axios
   .get(`http://t.me/${channel}`)
   .then(response => response.data)
   .then(data => /<div class="tgme_page_extra">(.*)? members?<\/div>/gi.exec(data))
   .then(matches => matches[1])
   .then(members => members.replace(' ', ''))
   .then(members => parseInt(members, 10))
+  .catch(() => fallback)
 
 module.exports = tmembers
