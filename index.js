4f065f80cf48aac1a77fdee095c6b1d4092df1e6
Igor Kamyshev <igor@kamyshev.me>
2019-02-23 12:20:10 +0200

    chore: replace linter by solid-scripts

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const axios = require('axios')
 
-const tmembers = (channel, fallback) => axios
-  .get(`http://t.me/${channel}`)
-  .then(response => response.data)
-  .then(data => /<div class="tgme_page_extra">(.*)? members?<\/div>/gi.exec(data))
-  .then(matches => matches[1])
-  .then(members => members.replace(' ', ''))
-  .then(members => parseInt(members, 10))
-  .catch(() => fallback)
+const tmembers = (channel, fallback) =>
+  axios
+    .get(`http://t.me/${channel}`)
+    .then(response => response.data)
+    .then(data =>
+      /<div class="tgme_page_extra">(.*)? members?<\/div>/gi.exec(data),
+    )
+    .then(matches => matches[1])
+    .then(members => members.replace(' ', ''))
+    .then(members => parseInt(members, 10))
+    .catch(() => fallback)
 
 module.exports = tmembers
